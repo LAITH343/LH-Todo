2640c3a915be27bd88a834b1aed7d5878f6337df
LAITH343 <87765501+LAITH343@users.noreply.github.com>
2022-09-21 16:36:44 +0300

    first step

---
+++ android/app/src/main/kotlin/com/example/lh_todo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lh_todo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:lh_todo/screens/home/home.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: HomeScreen(),
+  ));
+}
